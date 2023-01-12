f9993c9247fd3b056401e5dc5f4875ba362adde3
Marina Veretina <mar-r-rinka@mail.ru>
2023-01-12 16:36:16 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        List<Post> posts = null;
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+            System.out.println();
+            posts = mapper.readValue(
+                    response.getEntity().getContent(), new TypeReference<>() {
+                    });
+//            posts.forEach(System.out::println);
+            posts.stream().filter(value -> (value.getUpvotes() != 0 && value.getUpvotes() > 0)).forEach(System.out::println);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
